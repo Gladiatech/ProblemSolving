d2f7307cc7355dded1a4640379980bceec254d49
Fra <francesco.garelli@gmail.com>
2020-05-17 02:25:13 +0100

    First Commit on Problem Solving Repository

---
+++ Leetcode/Array_Questions.h
@@ -0,0 +1,40 @@
+#pragma once
+#include<vector>
+
+using namespace std;
+
+vector<int> input = { 0,0,1,1,1,2,2,3,3,4 };
+
+
+int removeDuplicates(vector<int>& nums) {
+
+	size_t arrayLength = nums.size();
+
+
+	int tmp = nums[0];
+	int counter = 0;
+	int k = arrayLength;
+
+	for (unsigned int i = 1; i < k; i++)
+	{
+
+		int current = 0;
+		if (i >= nums.size())break;
+		current = nums[i];
+		if (tmp == current)
+		{
+			nums.erase(nums.begin() + i);
+			i = 0;
+			tmp = nums[0];
+
+		}
+		else
+		{
+			tmp = nums[i];
+		}
+
+
+	}
+
+	return(nums.size());
+}
---
+++ Leetcode/Leetcode.cpp
@@ -0,0 +1,15 @@
+// Leetcode.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Array_Questions.h"
+
+int main()
+{
+
+    int result = removeDuplicates(input);
+
+    std::cout << "Hello World!\n";
+}
+
+
