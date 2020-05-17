2504830c715369062e5c7562d0ae94b376777fbb
Fra <francesco.garelli@gmail.com>
2020-05-17 03:19:38 +0100

    Added LinkedList problem file

---
+++ Leetcode/LinkedList.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <list>
+
+using namespace std;
+
+//Leetcode 206
+//Reverse Linked List
+
+struct ListNode
+{
+	int value;
+	ListNode* next;
+};
+
+
+ListNode* reverseList(ListNode* head) {
+
+
+
+
+
+
+}
